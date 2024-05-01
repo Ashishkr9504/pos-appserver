2e4dc47d0188c2c22e586de013ece00b7b080c9f
Ashish Kumar <137616225+Ashishkr9504@users.noreply.github.com>
2024-05-02 00:17:35 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const path= require("path")
+const { bgCyan } = require("colors");
+require("colors");
+const connectDb = require("./config/config");
+//dotenv config
+if(process.env.NODE_ENV!=="PRODUCTION"){
+  require("dotenv").config();
+}
+
+//db config
+connectDb();
+//rest object
+const app = express();
+
+//middlwares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/items", require("./routes/itemRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/bills", require("./routes/billsRoutes"));
+app.get("/",(req,res)=>{
+  res.send("<h1>POS BACKEND </h1>");
+});
+app.use(express.static(path.join(__dirname,"../client/build")));
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"..client//build/index.html"));
+})
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(`Server Running On Port ${PORT}`.bgCyan.white);
+});
