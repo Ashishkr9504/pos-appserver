3d2becd90950a91ba4735a5dd92e5d942bf45c90
Ashish Kumar <137616225+Ashishkr9504@users.noreply.github.com>
2024-05-12 09:55:22 +0530

    change

--- server.js
+++ server.js
@@ -19,11 +19,12 @@ const app = express();
 //middlwares
 app.use(
   cors({
-    origin: ["https://pos-client-bz8z.vercel.app/"],
-    methods: ["POST","GET"],
+    origin: "https://pos-client-31hv.vercel.app/",
+    origin:true,
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
